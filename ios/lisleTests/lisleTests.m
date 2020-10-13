82f44887938e317ba18e5cf9554cd1952530aed5
hukpavlo <pavlo2001lviv@gmail.com>
2020-10-13 23:48:37 +0300

    changed app name

--- ios/lisleTests/lisleTests.m
+++ ios/lisleTests/lisleTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface waflyTests : XCTestCase
+@interface lisleTests : XCTestCase
 
 @end
 
-@implementation waflyTests
+@implementation lisleTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
